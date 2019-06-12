df2696292133da4c60e94e6773f1f5100c3ad377
wgr <648848371@qq.com>
2019-06-12 21:31:44 +0800

    把本地工程添加到git

---
+++ git-test/src/cn/itcast/git/Tset.java
@@ -0,0 +1,9 @@
+package cn.itcast.git;
+
+public class Tset {
+    public static void main(String[] args) {
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+    }
+}
